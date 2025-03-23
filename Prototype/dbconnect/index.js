e07c2461fa8fc72662f0c5f9c355ac4e25d07e8b
KohZenYii <126935977+KohZenYii@users.noreply.github.com>
2025-03-23 15:00:40 +1100

    mongodb npm installed and coded to access mongodb from js

---
+++ Prototype/dbconnect/index.js
@@ -0,0 +1,26 @@
+const {MongoClient} = require('mongodb');
+const username = 'zkoh0011';
+const password = 'Amoskohzenyii';
+async function main() {
+    const url = "mongodb+srv://zkoh0011:Amoskohzenyii@cluster0.einni.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=Cluster0";
+
+    const client = new MongoClient(url);
+try{
+    await client.connect();
+
+await listDatabases(client);}
+    catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();   
+}
+
+}
+main().catch(console.error);
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
